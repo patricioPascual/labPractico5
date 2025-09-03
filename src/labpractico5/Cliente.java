e1cfbd0e7347eaf564d16ae72d7eabfab1c5930c
patricioPascual <patriciopascual2@gmail.com>
2025-09-02 21:33:55 -0300

    Initial commit

---
+++ src/labpractico5/Cliente.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labpractico5;
+
+/**
+ *
+ * @author patri
+ */
+public class Cliente {
+    private int dni;
+    private String nombre;
+    private String apellido; 
+    private String domicilio;
+
+    public Cliente(int dni, String nombre, String apellido, String domicilio) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.domicilio = domicilio;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+    
+    
+}
---
+++ src/labpractico5/DirectorioTelefonico.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labpractico5;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author patri
+ */
+public class DirectorioTelefonico {
+    
+    public  TreeMap<Long,Cliente> directorio;
+
+    public DirectorioTelefonico() {
+    }
+    
+    public void agregarContacto(Long numero , Cliente cliente){
+        
+        directorio.put(numero, cliente);
+    }
+    public Cliente buscarContacto(Long numero){
+        
+     if(directorio.containsKey(numero)){
+         return directorio.get(numero);
+     } 
+     return null;
+}
+    public Set buscarTelefono(String apellido){
+        Set<Long> listaNumeros = new HashSet();
+        for(Map.Entry<Long,Cliente>  aux : directorio.entrySet()){
+            Cliente clienteActual= aux.getValue();
+            if(apellido.equalsIgnoreCase(clienteActual.getApellido())){
+                listaNumeros.add(aux.getKey());
+            }
+            
+        }
+      return listaNumeros;
+    }
+}
